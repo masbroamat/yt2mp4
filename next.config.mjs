b48d3b62e9c24185ee1d4f76c8426e2c800f99a1
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2025-02-28 09:01:29 +0800

    fixing ytdlp issue

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    webpack: (config) => {
-      config.module = {
-        ...config.module,
-        exprContextCritical: false, // Suppress critical dependency warnings
-      };
-      return config;
-    },
-  };
