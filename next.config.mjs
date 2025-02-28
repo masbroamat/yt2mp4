1a3b70af32a94d973f3ceb28d21e963a7e76a654
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2025-02-28 08:25:09 +0800

    fixing ytdlp issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        // Exclude the Windows FFmpeg installer module from bundling.
+        config.externals = config.externals || [];
+        config.externals.push({
+          '@ffmpeg-installer/win32-x64': 'commonjs2 @ffmpeg-installer/win32-x64',
+        });
+        // (Optionally, add others if you target multiple OSes)
+        // config.externals.push({
+        //   '@ffmpeg-installer/darwin-x64': 'commonjs2 @ffmpeg-installer/darwin-x64',
+        // });
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
