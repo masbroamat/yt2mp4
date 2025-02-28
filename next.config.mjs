9cefdd10897d511103cfaa486056ae672a0fd517
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2025-02-28 08:37:25 +0800

    fixing ytdlp issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,11 @@ const nextConfig = {
     webpack: (config, { isServer }) => {
       if (isServer) {
         config.externals = config.externals || [];
-        // Tell webpack to treat the linux-x64 FFmpeg binary as an external dependency:
-        config.externals.push({
-          '@ffmpeg-installer/linux-x64': 'commonjs2 @ffmpeg-installer/linux-x64',
-        });
+        // Exclude all @ffmpeg-installer packages from bundling
+        config.externals.push(/^@ffmpeg-installer\/.*/);
       }
       return config;
     },
   };
   
   export default nextConfig;
-  
